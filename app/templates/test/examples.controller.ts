82cf73877feb8d87fbc09bd8ea2ff2ff81ccc437
Carmine DiMascio <dimascio@us.ibm.com>
2018-02-17 23:05:00 -0500

    add mocha tests

---
+++ app/templates/test/examples.controller.ts
@@ -0,0 +1,39 @@
+import 'mocha';
+import { expect } from 'chai';
+import * as request from 'supertest';
+import Server from '../server';
+
+describe('Examples', () => {
+  it('should get all examples', () =>
+    request(Server)
+      .get('<%= apiRoot %>/examples')
+      .expect('Content-Type', /json/)
+      .then(r => {
+        expect(r.body)
+          .to.be.an.an('array')
+          .of.length(2);
+      }));
+
+  it('should add a new example', () =>
+    request(Server)
+      .post('<%= apiRoot %>/examples')
+      .send({ name: 'test' })
+      .expect('Content-Type', /json/)
+      .then(r => {
+        expect(r.body)
+          .to.be.an.an('object')
+          .that.has.property('name')
+          .equal('test');
+      }));
+
+  it('should get an example by id', () =>
+    request(Server)
+      .get('<%= apiRoot %>/examples/2')
+      .expect('Content-Type', /json/)
+      .then(r => {
+        expect(r.body)
+          .to.be.an.an('object')
+          .that.has.property('name')
+          .equal('test');
+      }));
+});
