17481aad3c0b191b8fb0c43a68211d997ec54569
Carmine DiMascio <cdimascio@tripadvisor.com>
2019-11-30 21:03:03 -0500

    v8.0.0

--- app/templates/server/common/openapi.ts
+++ app/templates/server/common/openapi.ts
@@ -1,16 +1,22 @@
-import path from 'path';
-import express from 'express';
-import { OpenApiValidator } from 'express-openapi-validator';
+import * as path from 'path';
+import express, { Application } from 'express';
 import errorHandler from '../api/middlewares/error.handler';
+import { OpenApiValidator } from 'express-openapi-validator';
 
-export default function openapi(app, routes) {
-    const apiSpec = path.join(__dirname, 'api.yml');
-    app.use(process.env.OPENAPI_SPEC || '/spec', express.static(apiSpec));
-
-    new OpenApiValidator({
+export default function (app: Application, routes: (app: Application) => void): Promise<void> {
+  const apiSpec = path.join(__dirname, 'api.yml');
+  const validateResponses = !!(
+    process.env.OPENAPI_ENABLE_RESPONSE_VALIDATION &&
+    process.env.OPENAPI_ENABLE_RESPONSE_VALIDATION.toLowerCase() === 'true'
+  );
+  return new OpenApiValidator({
     apiSpec,
-    }).install(app);
-
-    routes(app);
-    app.use(errorHandler);
-}
+    validateResponses,
+  })
+    .install(app)
+    .then(() => {
+      app.use(process.env.OPENAPI_SPEC || '/spec', express.static(apiSpec));
+      routes(app);
+      app.use(errorHandler);
+    });
+};
