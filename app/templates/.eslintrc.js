e73e982873969ff099ee0855aadee37564687757
Harris Brakmic <brakmic@gmail.com>
2020-08-18 14:08:28 +0200

    fix: prettier crlf warning on windows

--- app/templates/.eslintrc.js
+++ app/templates/.eslintrc.js
@@ -12,5 +12,11 @@ module.exports = {
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
 };
