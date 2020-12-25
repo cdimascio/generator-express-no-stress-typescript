c6f3375f9a2c8ee5b55b8dad8225d63b6d0638c7
Carmine DiMascio <cdimasci@amazon.com>
2020-12-25 11:59:16 -0500

    fix: remove appPath

--- app/templates/server/common/server.ts
+++ app/templates/server/common/server.ts
@@ -18,7 +18,6 @@ export default class ExpressServer {
   private routes: (app: Application) => void;
   constructor() {
     const root = path.normalize(__dirname + '/../..');
-    app.set('appPath', root + 'client');
     app.use(bodyParser.json({ limit: process.env.REQUEST_LIMIT || '100kb' }));
     app.use(bodyParser.urlencoded({ extended: true, limit: process.env.REQUEST_LIMIT || '100kb' }));
     app.use(bodyParser.text({ limit: process.env.REQUEST_LIMIT || '100kb'}));
